4ef475d677bcfd35d5c6352a30d523162a755486
Abdurrahman Harmankaya <abdurrahman.kim@gmail.com>
2024-03-19 07:28:29 +0300

    Update helpers.php

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -2,7 +2,7 @@
 
 
 if (!function_exists('vextension_asset')) {
-    function voyager_asset($path, $secure = null)
+    function vextension_asset($path, $secure = null)
     {
         return route('voyager.vextension_assets').'?path='.urlencode($path);
     }
